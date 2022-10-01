3be4caf8a729cd115e5245f44f201899f69b7ff9
WeslleySantosln <weslleys.santana299@gmail.com>
2022-09-30 23:51:06 -0300

    Atualização 30092022!

--- app/src/main/java/com/example/mppandroid/FormLogin.java
+++ app/src/main/java/com/example/mppandroid/FormLogin.java
@@ -2,13 +2,47 @@ package com.example.mppandroid;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class FormLogin extends AppCompatActivity {
+
+   private TextView txtCasdastrar;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_form_login);
+
+        getSupportActionBar().hide();
+        IniciarComponentes();
+
+        txtCasdastrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(FormLogin.this,FormCadastro.class);
+                startActivity(intent);
+
+
+
+            }
+        });{
+
+
+
+        }
+
     }
+
+
+    private void IniciarComponentes(){
+
+           txtCasdastrar = findViewById(R.id.txtCadastrar);
+    }
+
+
+
 }
