9d400d131e4a05fa3af9302ca254b74f58b0971d
WeslleySantosln <weslleys.santana299@gmail.com>
2022-10-23 20:47:55 -0300

    Atualização 23102022

--- app/src/main/java/com/example/mppandroid/FormCadastro.java
+++ app/src/main/java/com/example/mppandroid/FormCadastro.java
@@ -1,10 +1,46 @@
 package com.example.mppandroid;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.snackbar.Snackbar;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException;
+import com.google.firebase.auth.FirebaseAuthUserCollisionException;
+import com.google.firebase.auth.FirebaseAuthWeakPasswordException;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import kotlin.collections.UCollectionsKt;
 
 public class FormCadastro extends AppCompatActivity {
+
+
+    private EditText editNome, editEmail, editSenha;
+    private Button bt_Cadastrar;
+    private Button bt_Voltar;
+    String[] mensagens = {"Por favor, preencha todos os campos!", "Parabens! Cadastro realizado com sucesso!"};
+    String usuarioID;
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -12,5 +48,150 @@ public class FormCadastro extends AppCompatActivity {
         setContentView(R.layout.activity_form_cadastro);
 
         getSupportActionBar().hide();
+        IniciarComponentes();
+
+        bt_Cadastrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String nome = editNome.getText().toString();
+                String email = editEmail.getText().toString();
+                String senha = editSenha.getText().toString();
+
+                if(nome.isEmpty() || email.isEmpty() || senha.isEmpty()){
+                    Snackbar snackbar = Snackbar.make(v,mensagens[0],Snackbar.LENGTH_SHORT );
+                    snackbar.setBackgroundTint(Color.WHITE);
+                    snackbar.setTextColor(Color.BLACK);
+                    snackbar.show();
+
+                }else{
+                   CadastrarUsuario(v);
+
+                }
+
+            }
+        });
+
+        bt_Voltar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent(FormCadastro.this,FormLogin.class);
+                startActivity(intent);
+                finish();
+
+
+            }
+        });
+
+
+
+
     }
+
+    private void CadastrarUsuario(View v){
+
+        String email = editEmail.getText().toString();
+        String senha = editSenha.getText().toString();
+
+        Task<AuthResult> authResultTask = FirebaseAuth.getInstance().createUserWithEmailAndPassword(email, senha).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+
+                if (task.isSuccessful()) {
+
+                    SalvarDadosUsuario();
+
+                    Snackbar snackbar = Snackbar.make(v, mensagens[1], Snackbar.LENGTH_SHORT);
+                    snackbar.setBackgroundTint(Color.WHITE);
+                    snackbar.setTextColor(Color.BLACK);
+                    snackbar.show();
+
+
+
+
+
+
+
+
+                }else{
+                    String erro;
+                    try {
+                        throw task.getException();
+
+                    }catch(FirebaseAuthWeakPasswordException e) {
+
+                        erro = "Senha fraca, digite uma senha com no mínimo 6 caracteres";
+
+                    }catch(FirebaseAuthUserCollisionException e) {
+
+                        erro = "E-mail já cadastrado";
+
+                    }catch (FirebaseAuthInvalidCredentialsException e){
+
+                        erro = "E-mail inválido";
+
+                    }catch (Exception e){
+
+                        erro = "Erro ao cadastrar usuário, por favor, verifique sua conexão!";
+
+                    }
+
+                    Snackbar snackbar = Snackbar.make(v, erro, Snackbar.LENGTH_SHORT);
+                    snackbar.setBackgroundTint(Color.WHITE);
+                    snackbar.setTextColor(Color.BLACK);
+                    snackbar.show();
+
+
+
+                }
+
+
+            }
+
+
+        });
+
+    }
+
+    private void SalvarDadosUsuario(){
+        String nome = editNome.getText().toString();
+
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+
+        Map<String,Object> usuarios = new HashMap<>();
+        usuarios.put("nome",nome);
+
+        usuarioID = FirebaseAuth.getInstance().getCurrentUser().getUid();
+
+        DocumentReference documentReference = db.collection("Usuarios").document(usuarioID);
+
+        documentReference.set(usuarios).addOnSuccessListener(new OnSuccessListener<Void>() {
+            @Override
+            public void onSuccess(Void aVoid) {
+                Log.d("db","Sucesso ao salvar os dados");
+
+            }
+        })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.d("db_error","Erro ao salvar os dados" + e.toString());
+                    }
+                });
+    }
+
+    private void IniciarComponentes(){
+        editNome = findViewById(R.id.editNome);
+        editEmail = findViewById(R.id.editEmail);
+        editSenha = findViewById(R.id.editSenha);
+        bt_Cadastrar = findViewById(R.id.bt_Cadastrar);
+        bt_Voltar = findViewById(R.id.bt_Voltar);
+
+
+
+    }
+
+
+
 }
